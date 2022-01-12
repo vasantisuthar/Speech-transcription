56573c7a92c2c9f25279e02035a4808831f132aa
Vasanti Suthar <vasantisuthar15@gmail.com>
2022-01-13 01:17:03 +0530

    added deepgram api for speech to text functionality

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+require('dotenv').config();
+const audioFileUrl = "https://static.deepgram.com/examples/deep-learning-podcast-clip.wav"
 const express = require('express');
+const {Deepgram} = require('@deepgram/sdk');
 const app = express();
+
+const deepgram = new Deepgram(process.env.dg_key)
+deepgram.transcription.preRecorded({url: audioFileUrl}).then(data =>{
+    console.dir(data,{depth:null});
+})
+
 
 app.use(express.static("public"));
 
+app.get('/',(req, res) =>{
+    res.sendFile(__dirname + '/index.html')
+})
 app.listen(3000,() =>{
     console.log("server at port 3000");
 })
